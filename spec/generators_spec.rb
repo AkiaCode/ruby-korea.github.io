d90a634f0306cf938e4250241457273e43bcf16e
Shim Won <marocchino@Wons-MacBook-Air.local>
2015-01-18 14:56:25 +0900

    ruby-korea/ruby-korea.github.io@7a7e255f222c1bbf08ee0f7e28264fe5b2cc8d4b

--- spec/generators_spec.rb
+++ spec/generators_spec.rb
@@ -1,33 +1,5 @@
-require 'spec_helper'
-describe RSSGenerator do
-  subject(:generator) { RSSGenerator.new }
-  it { is_expected.not_to be_nil }
-end
+describe RSS::Generator do
+  it { expect(true).to eq true }
 
-describe Article do
-  describe ".all" do
-    subject(:articles) { Article.all([
-        '- [title](https://link.to.article){: .article data-date="2015.01.12" data-tags="translated ruby" data-author="author" }',
-        '- [title](https://link.to.article){: .article data-date="2015.01.12" data-tags="ruby" data-author="author" }',
-      ]) }
-    it { expect(articles.size).to be(2) }
-  end
 
-  describe "#new" do
-    subject(:article) { Article.new(row) }
-    context 'when translated row' do
-      let(:row) { '- [title](https://link.to.article){: .article data-date="2015.01.12" data-tags="translated ruby" data-author="author" }' }
-      it { expect(subject.to_h["title"]).to eq("[번역] title") }
-    end
-
-    context 'when normal row' do
-      let(:row) { '- [title](https://link.to.article){: .article data-date="2015.01.12" data-tags="ruby" data-author="author" }' }
-      it { expect(subject.to_h["title"]).to eq("title") }
-    end
-
-    context 'when invalid row' do
-      let(:row) { '' }
-      it { expect { article }.to raise_error Article::InvalidRow }
-    end
-  end
 end
